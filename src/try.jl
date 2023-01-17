3cbce77253cd11409a1454c20b594a3b60006f44
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-01-17 19:22:27 +0800

    feat(cal_imgsVideo): process video directly [require ffmpeg for total frame]

--- src/try.jl
+++ src/try.jl
@@ -39,6 +39,60 @@ function cal_imgsfol(fol, config=[4,5]; createResultFolder=false, res_postfix="_
     return fname_list, corners_list
 end
 
+using VideoIO, Images, ImageDraw
+function cal_imgsVideo(vidfname, config=[4,5]; resImageFilepath=false, res_postfix="_res_"*replace( string(config), '['=>'(', ']'=>')', ','=>'.' ) )
+    vid = VideoIO.openvideo(vidfname) #"/Users/abel/Documents/aspod/data/2022-05-13_nus-pool/0015/Vid_20131219_101550.mkv")
+
+    config = cv.Size(Int32(config[1]),Int32(config[2]))
+    @info "counting total frames..."
+    # totalframe = counttotalframes(vid)
+    try
+        totalframe = readchomp(`ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1  $vidfname`)
+        totalframe = parse(Int, totalframe)
+    catch err
+        totalframe = missing
+        @info "[ERR]unable to get total number of frame"
+    end
+    corners_list = []; frame_list = []
+    counts=1
+    while !eof(vid)
+        img = read(vid);
+
+        config = cv.Size(Int32(4),Int32(5))
+        dot_size = 25
+
+        img_cv = permutedims(channelview(img), (1,3,2))
+        img_cv = cv.Mat{UInt8}(Any, UInt8.(round.(img_cv.*255)) )
+        gray = cv.cvtColor(img_cv, cv.COLOR_BGR2GRAY);
+        ret, corners = cv.findChessboardCorners(gray, config, flags=cv.CALIB_CB_EXHAUSTIVE)
+        if ret
+            corners_jl = Int.(round.(corners))
+            # for i in 1:size(corners_jl,3)
+            #     draw!(img, Ellipse(CirclePointRadius(corners_jl[1,1,i], corners_jl[2,1,i],dot_size)), RGB{N0f8}(1,0,0))
+            # end
+            push!(corners_list, corners_jl[:,1,:])
+            push!(frame_list, counts)
+        end
+        @debug(string(counts) * "/" * string(totalframe) *"\t"* string(ret) )
+        counts += 1
+    end
+
+    img
+    for j in 1:length(corners_list)
+        corner = corners_list[j]
+        for i in 1:size(corner,2)
+            draw!(img, Ellipse(CirclePointRadius(corner[1,i], corner[2,i],dot_size)), RGB{N0f8}(1,0,0))
+            #@debug [corner[1,i], corner[2,i]]
+        end
+    end
+    display(img)
+    
+    return img, corners_list
+end
+
+@info ("fully loaded")
+
+
 # f,c_2 = cal_imgsfol("/Users/abel/Documents/data_res/aspod/aspod2_20220513_nuspool", [4,5]; createResultFolder=true)
 # f,c = cal_imgsfol("/Users/abel/Documents/data_res/calf/HK_camcalib", [6,9]; createResultFolder=true)
 # /Users/abel/Documents/data/calf/Calibration/Checkerboard_Calibration/20191128/20191128_14.40.35_log.mkv
